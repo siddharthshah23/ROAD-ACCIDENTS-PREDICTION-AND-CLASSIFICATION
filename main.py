34bd27a179d015f3dafc46538d1228a998158f99
Sid Shah <siddharthshah23@outlook.com>
2023-10-06 10:45:32 -0400

    fixed code and added requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import pandas as pd
-from sklearn.externals import joblib
+import joblib
 import numpy as np
 import urllib.request
 import urllib.parse
